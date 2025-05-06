444f33c0d512713797f1ff2886052aa937cd78c9
AKAjaikumar <leelaajaikumar@gmail.com>
2025-05-06 20:04:03 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -11,48 +11,42 @@ require([
     'DataGridView',
     'WAFData'
 ], function (UWA, DataGridView, WAFData) {
-    // Wait for the platform to load the widget
-    if (typeof widget !== 'undefined' && widget.addEvent) {
-        widget.addEvent('onLoad', function () {
-            console.log("Widget Loaded");
+   return {
+        onLoad: function () {
+            console.log("onLoad triggered");
 
+            // Create container
             var container = document.createElement('div');
-            document.body.appendChild(container);
-            container.style.padding = '10px';
+            container.style.width = '100%';
+            container.style.height = '100%';
+            widget.body.appendChild(container);
 
-            var gridView = new DataGridView({
-                columns: [
-                    { text: 'Name', dataIndex: 'name', sortable: true, width: '150px' },
-                    { text: 'Type', dataIndex: 'type', sortable: true, width: '120px' },
-                    { text: 'Revision', dataIndex: 'revision', sortable: true, width: '100px' }
-                ]
+            // Create and render DataGridView
+            DataGridView().then(function (gridView) {
+                var config = new DataGridViewConfig({
+                    id: 'demo-grid',
+                    enableSorting: true
+                });
+
+                var layout = new DataGridViewLayoutEngine({
+                    columns: [
+                        { text: 'Name', dataIndex: 'name', width: 200 },
+                        { text: 'Type', dataIndex: 'type', width: 100 }
+                    ]
+                });
+
+                gridView.setConfig(config);
+                gridView.setLayoutEngine(layout);
+                gridView.setData([{
+                    name: 'Document A',
+                    type: 'doc'
+                }, {
+                    name: 'Document B',
+                    type: 'pdf'
+                }]);
+
+                gridView.inject(container);
             });
-
-            gridView.inject(container);
-
-            WAFData.authenticatedRequest('/resources/v1/modeler/documents', {
-                method: 'GET',
-                type: 'json',
-                onComplete: function (data) {
-                    if (data && data.member) {
-                        var rows = data.member.map(function (doc) {
-                            return {
-                                name: doc.name,
-                                type: doc.type,
-                                revision: doc.revision
-                            };
-                        });
-                        gridView.addRows(rows);
-                    } else {
-                        console.error('No documents returned');
-                    }
-                },
-                onFailure: function (error) {
-                    console.error('Failed to fetch documents:', error);
-                }
-            });
-        });
-    } else {
-        console.error('widget object is not available');
-    }
+        }
+    };
 });
