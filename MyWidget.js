2c269791c89c42ee7f4b2df02f0a569924b1a876
AKAjaikumar <leelaajaikumar@gmail.com>
2025-05-06 17:35:53 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -1,14 +1,36 @@
 require([
     'UWA/Core',
-    'ExternalDocument/DataGridView/DataGridView'
-], function(UWA, DataGridView) {
+    'enovia/DataGridView/DataGridView',
+    'DS/TreeModel/TreeDocument',
+    'DS/TreeModel/TreeNodeModel'
+], function(UWA, DataGridView, TreeDocument, TreeNodeModel) {
     var myWidget = {
         onLoad: function() {
-		   console.log("Loaded");
-	}
-	};
+            console.log("Loaded");
+
+            var treeDocument = new TreeDocument();
+
+            var gridView = new DataGridView({
+                treeDocument: treeDocument,
+                columns: [
+                    { text: "Name", dataIndex: "name" },
+                    { text: "Type", dataIndex: "type" }
+                ]
+            });
+
+            var node = new TreeNodeModel({
+                label: "Sample",
+                grid: {
+                    name: "Sample Doc",
+                    type: "Document"
+                }
+            });
+
+            treeDocument.addRoot(node);
+            widget.body.empty().addContent(gridView);
+        }
+    };
 
     widget.addEvent('onLoad', myWidget.onLoad);
 });
-
 
