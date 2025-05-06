0430e3d4786e9a5abe0136573b8503bbebc4e1ba
AKAjaikumar <leelaajaikumar@gmail.com>
2025-05-06 17:52:44 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -1,41 +1,10 @@
 require([
     'UWA/Core',
-    'DS/DataGridView/DataGridView',
-    'DS/TreeModel/TreeDocument',
-    'DS/TreeModel/TreeNodeModel'
-], function(UWA, DataGridView, TreeDocument, TreeNodeModel) {
+    'DS/DataGridView/DataGridView'
+], function(UWA, DataGridView) {
     var myWidget = {
         onLoad: function() {
             console.log("Widget Loaded");
-
-            // Create a new TreeDocument
-            var treeDocument = new TreeDocument();
-
-            // Create a new grid view and pass the tree document and columns
-            var gridView = new DataGridView({
-                columns: [
-                    { text: "Name", dataIndex: "name" },
-                    { text: "Type", dataIndex: "type" }
-                ]
-            });
-
-            // Create a node to be added to the treeDocument
-            var node = new TreeNodeModel({
-                label: "Sample Node",
-                grid: {
-                    name: "Sample Doc",
-                    type: "Document"
-                }
-            });
-
-            // Add the node as the root of the tree
-            treeDocument.addRoot(node);
-
-            // Assuming you want to display data in the grid, you might need to bind this data
-            gridView.setData(treeDocument.getNodes());
-
-            // Add the grid to the widget body
-            widget.body.empty().addContent(gridView);
         }
     };
 
