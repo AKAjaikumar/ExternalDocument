2d51c750bdceb674c0fa80079b2babd870cec897
AKAjaikumar <leelaajaikumar@gmail.com>
2025-05-06 20:45:44 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -3,14 +3,22 @@ define('MyWidget', [
     'DS/DataGridView/DataGridView',
     'DS/WAFData/WAFData'
 ], function (UWA, DataGridView, WAFData) {
+    'use strict';
+
     var myWidget = {
         onLoad: function () {
             console.log("Widget Loaded");
+
             DataGridView().then(function (gridView) {
                 console.log("DataGridView ready");
+                // TODO: Add DataGridView usage logic here
+            }).catch(function (error) {
+                console.error("Failed to load DataGridView:", error);
             });
         }
     };
 
     widget.addEvent('onLoad', myWidget.onLoad);
+
+    return myWidget;
 });
