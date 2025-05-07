fe2a2b4f4c11da9dee93b795c9abe4297fc87834
AKAjaikumar <leelaajaikumar@gmail.com>
2025-05-07 13:11:02 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -16,7 +16,7 @@ require([
 			var spaceURL = widget.getValue('x3dSpaceURL');
 			console.log("spaceURL:", spaceURL);
 			//var url = '/api/' + platformId + '/enovia/resources/v1/modeler/documents';
-            var url = spaceURL + '/enovia/resources/v1/modeler/documents';
+            var url = '/enovia/resources/v1/modeler/documents';
             var container = document.createElement('div');
             document.body.appendChild(container);
             container.style.padding = '10px';
