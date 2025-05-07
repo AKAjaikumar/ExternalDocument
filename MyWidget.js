76ac920c81c25f5dc6fd362752a1026ff48897bd
AKAjaikumar <leelaajaikumar@gmail.com>
2025-05-07 10:03:46 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -1,15 +1,7 @@
-require.config({
-    baseUrl: 'https://akajaikumar.github.io/ExternalDocument/',
-    paths: {
-        'DataGridView': 'DS/DataGridView/DataGridView',
-        'WAFData': 'DS/WAFData/WAFData'
-    }
-});
-
 require([
     'UWA/Core',
-    'DataGridView',
-    'WAFData'
+    'DS/DataGridView/DataGridView',
+    'DS/WAFData/WAFData'
 ], function (UWA, DataGridView, WAFData) {
     // Wait for 3DEXPERIENCE platform to fully load the widget
     if (typeof widget !== 'undefined') {
@@ -22,6 +14,46 @@ require([
             document.body.appendChild(container);
             container.style.padding = '10px';
 
+            
+            var gridView = new DataGridView({
+                columns: [
+                    { text: 'Name', dataIndex: 'name', sortable: true, width: '150px' },
+                    { text: 'Type', dataIndex: 'type', sortable: true, width: '120px' },
+                    { text: 'Revision', dataIndex: 'revision', sortable: true, width: '100px' }
+                ]
+            });
+
+            // Inject the gridView into the container
+            gridView.inject(container);
+
+            // Make authenticated request to fetch documents from the 3DEXPERIENCE services
+            WAFData.authenticatedRequest('/resources/v1/modeler/documents', {
+                method: 'GET',
+                type: 'json',
+                onComplete: function (data) {
+                    if (data && data.member) {
+                        var rows = data.member.map(function (doc) {
+                            return {
+                                name: doc.name,
+                                type: doc.type,
+                                revision: doc.revision
+                            };
+                        });
+                        // Add rows to the DataGridView
+                        gridView.addRows(rows);
+                    } else {
+                        console.error('No documents returned');
+                    }
+                },
+                onFailure: function (error) {
+                    console.error('Failed to fetch documents:', error);
+                }
+            });
+        });
+    } else {
+        console.error('Widget object is not available');
+    }
+});
             
             var gridView = new DataGridView({
                 columns: [
