454d5d5215e58d76c25bd5e9010d0495562a66e9
AKAjaikumar <leelaajaikumar@gmail.com>
2025-05-07 09:07:59 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -1,17 +1,26 @@
-require([
+define('MyWidget', [
     'UWA/Core'
-], function(UWA) {
+], function (UWA) {
+    'use strict';
+
     var myWidget = {
-        onLoad: function() {
-			console.log("Widget loaded from JS !");
+        onLoad: function () {
+            console.log("Widget loaded from JS!");
             var container = widget.body;
             container.innerHTML = '';
+
             if (!container) {
-                console.error("Container #testGridView not found!");
+                console.error("Container not found!");
                 return;
             }
-			
-		}
-	};
-	widget.addEvent('onLoad', myWidget.onLoad);
+
+            container.innerHTML = '<div style="padding:10px;">Hello from 3DEXPERIENCE widget!</div>';
+        }
+    };
+
+    if (typeof widget !== 'undefined') {
+        widget.addEvent('onLoad', myWidget.onLoad);
+    }
+
+    return myWidget;
 });
