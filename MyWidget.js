b4ebbca9b229ae9e24dca9d6cd9cd07a672cff54
AKAjaikumar <leelaajaikumar@gmail.com>
2025-05-19 12:37:59 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -752,6 +752,7 @@ require([
 										type: 'json',
 										headers: {
 											'Content-Type': 'application/json',
+											'SecurityContext': 'VPLMProjectLeader.Company Name.APTIV INDIA',
 											[csrfHeaderName]: csrfToken
 										},
 										data: JSON.stringify(payload),
