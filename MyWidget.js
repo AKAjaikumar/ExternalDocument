8f657ff62f146d189d5db5b6f8d86f5475dcf9ee
AKAjaikumar <leelaajaikumar@gmail.com>
2025-05-23 16:30:18 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -576,7 +576,7 @@ require([
 						const csrfToken = csrfData.csrf.value;
 						const csrfHeaderName = csrfData.csrf.name;
 
-						const ecosystemURL = baseUrl + '/resources/v1/modeler/documents/' + documentId + '';
+						const ecosystemURL = baseUrl + '/resources/v1/modeler/documents/' + documentId + '?$include=[!]parents';
 
 						WAFData.authenticatedRequest(ecosystemURL, {
 						  method: 'GET',
