7f3f8abd315f76d3316b485875347bfa858f6f5c
AKAjaikumar <leelaajaikumar@gmail.com>
2025-05-23 17:21:51 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -589,7 +589,7 @@ require([
 						  },
 						  onComplete: function (response) {
 							if (response && response.data && response.data.length > 0) {
-                                    resolve(response.data[0].id;);
+                                    resolve(response.data[0].id);
 							} else {
 								reject("No data found for document.");
 							}
